175610043450f12fc4a993d1af778e120297375e
Femke Knottenbelt <femknotgithub@gmail.com>
2018-11-12 15:45:55 +0000

    Add empty string IP option

--- lesson_03/server.rb
+++ lesson_03/server.rb
@@ -7,10 +7,13 @@ loop do
   request_line = client.gets
   puts request_line
 
+  client.puts "HTTP/1.1 200 ok"
+  client.puts "Content-Type: text/plain\r\n\r\n"
+
   client.puts request_line
   client.close
 end
 
-# nb: replace localhost with droplet IP and port
+# nb: replace localhost with droplet IP (or just "") and port
 # with a open tcp port when running on droplet.
 # in browser go to http://<ip>:<port>

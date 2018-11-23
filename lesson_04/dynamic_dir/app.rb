5c91875c1a663f31ea69cda1497e1467d471340c
Femke Knottenbelt <femknotgithub@gmail.com>
2018-11-23 13:18:25 +0000

    Finish Code Challenge: Dynamic Directory Index

--- lesson_04/dynamic_dir/app.rb
+++ lesson_04/dynamic_dir/app.rb
@@ -4,12 +4,22 @@ require "tilt/erubis"
 require "find"
 
 get "/" do
+
   @files = []
+  @reverse = params[:reverse]
 
   path_to_search = "./public"
   Find.find(path_to_search) do |file|
     @files << File.basename(file) if File.file?(file)
   end
 
+  if @reverse == 'true'
+    @files.sort!.reverse!
+  else
+    @files.sort!
+  end
+
+  @info = ['params', params, "files", @files]
   erb :home
 end
+

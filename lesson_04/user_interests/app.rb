fcb41ac97a9464b736a88d5991d80e638f04e0cf
Femke Knottenbelt <femknotgithub@gmail.com>
2018-11-27 18:11:35 +0000

    Add basic user page link

--- lesson_04/user_interests/app.rb
+++ lesson_04/user_interests/app.rb
@@ -3,12 +3,19 @@ require "sinatra/reloader"
 require "tilt/erubis"
 require "yaml"
 
+before do
+  @users = YAML.load_file "users.yaml"
+end
 
 get "/" do
   redirect "/users"
 end
 
 get "/users" do
-  @users = YAML.load_file "users.yaml"
   erb :users
+end
+
+get "/:name" do
+  @name = params[:name]
+  erb :user_page
 end
